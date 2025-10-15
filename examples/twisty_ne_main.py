f4cb6a83d823841b0c685be305e747dd6df16be9
Fullitu <k.chmielewski@poczta.onet.pl>
2025-10-15 13:44:22 +0200

    Remove unused functions and refactor fitness calculation
    Deleted the unused 'run' and 'policy' functions and commented-out code to clean up the script. Refactored 'fitness_function_basic' to only consider x-axis distance for fitness calculation.

--- examples/twisty_ne_main.py
+++ examples/twisty_ne_main.py
@@ -78,18 +78,6 @@ SPAWN_POS = [0, 0, 0]
 NUM_OF_MODULES = 30
 # TARGET_POSITION = [5, 0, 0.5]
 
-# def fitness_function_olympics(history: list[tuple[float, float, float]]) -> float:
-#     """Calculate fitness based on robot's trajectory history.
-
-#     xt, yt, zt = TARGET_POSITION
-#     xc, yc, zc = history[-1]
-
-#     # Minimize the distance --> maximize the negative distance
-#     cartesian_distance = np.sqrt(
-#         (xt - xc) ** 2 + (yt - yc) ** 2 + (zt - zc) ** 2,
-#     )
-#     return -cartesian_distance
-
 class EASettings(BaseSettings):
 
     # world: SimpleFlatWorld = SimpleFlatWorld()
@@ -114,14 +102,12 @@ class EASettings(BaseSettings):
 config = EASettings()
 
 def fitness_function_basic(history: list[float]) -> float:
-    xs, ys, _ = config.starting_pos
-    xe, ye, _ = history[-1]
+    xs, _, _ = config.starting_pos
+    xe, _, _ = history[-1]
 
     # maximize the distance
-    cartesian_distance = np.sqrt(
-        (xs - xe) ** 2 + (ys - ye) ** 2,
-    )
-    return cartesian_distance
+    x_distance = xs - xe
+    return x_distance
 
 def create_individual(con_twisty: bool) -> Individual:
     ind = Individual()
@@ -205,143 +191,6 @@ def main() -> None:
         # Simulate the robot
         # run(core, ind)
 
-def run(
-    robot: CoreModule,
-    individual: Individual,
-    mode: str = "launcher",
-) -> None:
-    """Entry point."""
-    # BugFix -> "Python exception raised"
-    mujoco.set_mjcb_control(None)
-    print("Number of actuators:")
-    print(copy.copy(robot).spec.compile().nu)
-
-    # mujoco.set_mjcb_control(None)
-
-    # # MuJoCo configuration
-    # viz_options = mujoco.MjvOption()  # visualization of various elements
-
-    # # Visualization of the corresponding model or decoration element
-    # viz_options.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = True
-    # viz_options.flags[mujoco.mjtVisFlag.mjVIS_ACTUATOR] = True
-    # viz_options.flags[mujoco.mjtVisFlag.mjVIS_BODYBVH] = True
-
-    # MuJoCo basics
-    world = SimpleFlatWorld()
-
-    # Set random colors for geoms
-    for i in range(len(robot.spec.geoms)):
-        robot.spec.geoms[i].rgba[-1] = 0.5
-
-    # Spawn the robot at the world
-    world.spawn(robot.spec, spawn_position=SPAWN_POS)
-
-    # Compile the model
-    model = world.spec.compile()
-    data = mujoco.MjData(model)
-
-    # Reset state and time of simulation
-    mujoco.mj_resetData(model, data)
-
-    # Save the model to XML
-    xml = world.spec.to_xml()
-    with (DATA / f"{SCRIPT_NAME}.xml").open("w", encoding="utf-8") as f:
-        f.write(xml)
-
-    # Number of actuators and DoFs
-    console.log(f"DoF (model.nv): {model.nv}, Actuators (model.nu): {model.nu}")
-
-    # Define action specification and set policy
-    data.ctrl = RNG.normal(scale=0.1, size=model.nu)
-
-    # Actuators and CPG
-    mujoco.set_mjcb_control(None)
-    weight_matrix = RNG.uniform(-0.1, 0.1, size=(model.nu, model.nu))
-    cpg = CPGSensoryFeedback(
-        num_neurons=int(model.nu),
-        sensory_term=-0.0,
-        _lambda=0.01,
-        coupling_weights=weight_matrix,
-    )
-    cpg.reset()
-    # add brain genotype to the individual
-    individual.brain_genotype = cpg.c
-
-    # Initialize robot tracker
-    mujoco_type_to_find = mujoco.mjtObj.mjOBJ_GEOM
-    name_to_bind = "core"
-    tracker = Tracker(
-        mujoco_obj_to_find=mujoco_type_to_find,
-        name_to_bind=name_to_bind,
-    )
-    tracker.setup(world.spec, data)
-
-    # Initialize controller
-    ctrl = Controller(
-        controller_callback_function=policy,
-        time_steps_per_ctrl_step=1,
-        tracker=tracker,
-    )
-
-    mujoco.set_mjcb_control(lambda m, d: ctrl.set_control(m, d, cpg))
-
-    console.log(f"xpos before sim: {tracker.history["xpos"][0]}") # TODO: REMOVE DEBUG
-
-    match mode:
-        # Launches interactive viewer
-        case "launcher":
-            viewer.launch(
-                model=model,
-                data=data,
-            )
-
-        # This disables visualisation (fastest option)
-        case "simple_runner":
-            simple_runner(
-                model,
-                data,
-                duration=30,
-            )
-
-        # Records video of the simulation
-        case "video":
-            path_to_video_folder = str(DATA / "videos")
-            video_recorder = VideoRecorder(output_folder=path_to_video_folder)
-
-            # Render with video recorder
-            video_renderer(
-                model,
-                data,
-                duration=30,
-                video_recorder=video_recorder,
-            )
-        
-        case _:
-            console.log(f"Mode '{mode}' not recognized. No simulation run.")
-    print(20 * "-")
-    print("NU vs QPOS")
-    print(model.nu)
-    print(len(data.qpos))
-    print(20 * "-")
-    # return fitness_function(tracker.history["xpos"])
-
-def policy(
-    model: mujoco.MjModel,  # noqa: ARG001
-    data: mujoco.MjData,
-    cpg: CPGSensoryFeedback,
-) -> np.ndarray:
-    """Use feedback term to shift the output of the CPGs."""
-    x, _ = cpg.step()
-    return x * np.pi / 2
-
 if __name__ == "__main__":
     # Test several times
     main()
-
-# if __name__ == "__main__":
-    # world = SimpleFlatWorld()
-    # robot = construct_mjspec_from_graph(create_individual(con_twisty=True).genotype)
-    # print(robot.spec.compile().nu)
-
-    # data = mujoco.MjData(model)
-    # print(data.qpos)
